423d68e71c64188e9aa3319a0a0591aedba1a5a2
Pavan vasa <87085355+Pawanvasa@users.noreply.github.com>
2024-10-18 19:10:19 +0530

    add social media links (#5)
    Co-authored-by: Pavan-Coditas <pavan.vasa@coditas.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,24 @@ import { FaSquareXTwitter } from "react-icons/fa6"; // Correcting FaSquareXTwitt
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
-      </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+    <div className="flex flex-shrink-0 items-center">
+      <img className="mx-2 w-10" src={logo} alt="logo" />
+    </div>
+    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <a href="https://www.linkedin.com/in/pavanvasa" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
         <FaLinkedin />
+      </a>
+      <a href="https://github.com/Pawanvasa" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
         <FaGithub />
+      </a>
+      <a href="https://www.instagram.com/pavan_vasa/profilecard/?igsh=Y3J2cnVibDFyM3lw" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
         <FaInstagram />
+      </a>
+      <a href="https://twitter.com/your-profile" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
         <FaSquareXTwitter />
-      </div>
-    </nav>
+      </a>
+    </div>
+  </nav>
   );
 };
 
