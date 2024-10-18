3f071ab73d8140970ef225ee56e2279c0a7f4023
Pavan-Coditas <pavan.vasa@coditas.com>
2024-10-18 12:18:00 +0530

    data modified

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -33,7 +33,7 @@ const Experience = () => {
                                 {experience.description}
                             </p>
                             {experience.technologies.map((tech, index) => (
-                                <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">
+                                <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium">
                                     {tech}
                                 </span>
                             ))}
